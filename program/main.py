a6f5675306cc85541e3abfe47fe7660a99f672b6
Dmitrii Kalenov <drkalenov@gmail.com>
2024-03-04 00:40:54 +0700

    first commit

---
+++ program/main.py
@@ -0,0 +1,13 @@
+from func_connections import connect_dydx
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_private import abort_all_positions
+from func_public import construct_market_prices
+
+
+if __name__ == '__main__':
+    print("It works")
+    # Connect to client
+    try:
+        pass
+    except Exception as e:
+        pass
